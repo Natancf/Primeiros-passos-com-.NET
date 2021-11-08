12fc1f1b5ec456f0fa8adf0d62569fef5d4bc116
Natancf <202017728@aluno.unb.br>
2021-11-07 23:31:09 -0300

    restante das funcionalidades básicas aula 2

--- Aula_2/Program.cs
+++ Aula_2/Program.cs
@@ -80,6 +80,86 @@ namespace Aula_2
                 Console.WriteLine(s);
             }
         }
+
+        static void InstrucaoBreak(string[] args)
+        {
+            while (true)
+            {
+                string s = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(s))
+                    break;
+                Console.WriteLine(s);
+            }
+        }
+
+        static void InstrucaoContinue(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("/"))
+                    continue;
+
+                Console.WriteLine(args[i]);
+            }
+        }
+
+        static void InstrucaoReturn()
+        {
+            int Somar(int a, int b)
+            {
+                return a + b;
+            }
+
+            Console.WriteLine(Somar(1, 2));
+            Console.WriteLine(Somar(3, 4));
+            Console.WriteLine(Somar(5, 6));
+            return;
+        }
+
+        static void InstrucaoTryCatchFinallyThrow(string[] args)
+        {
+            double Dividir(double x, double y)
+            {
+                if (y == 0)
+                    throw new DivideByZeroException();
+                return x / y;
+            }
+
+            try
+            {
+                if(args.Length != 2)
+                {
+                    throw new InvalidOperationException("informe 2 números");
+                }
+
+                double x = double.Parse(args[0]);
+                double y = double.Parse(args[1]);
+                Console.WriteLine(Dividir(x, y));
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro genérico: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Até breve!");
+            }
+        }
+
+        static void InstrucaoUsing(string[] args)
+        {
+            using (System.IO.TextWriter w = System.IO.File.CreateText("teste.txt"))
+            {
+                w.WriteLine("Line 1");
+                w.WriteLine("Line 2");
+                w.WriteLine("Line 3");
+            }
+        }
     }
 
     
